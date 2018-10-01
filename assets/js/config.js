85bf60db17e09c8f0da7a3e6f9aca3dd576ff93c
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-10-01 22:39:36 +0200

    Remove firebase config

--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  apiKey: "AIzaSyC2F-E53e95idiMuZip6SM_-AkOrhr9Qnw",
-  authDomain: "character-generator-134dc.firebaseapp.com",
-  databaseURL: "https://character-generator-134dc.firebaseio.com",
-  projectId: "character-generator-134dc",
-  storageBucket: "character-generator-134dc.appspot.com",
-  messagingSenderId: "568892504074"
-}
